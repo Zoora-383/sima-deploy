c9550cc55d168d0c8feb3772840fa3113d727443
unknown <abdulganihadiansyah@gmail.com>
2026-06-03 19:55:50 +0700

    Feat: finalitation autentication feature, Fix: logic authentication

--- app/Http/Requests/Auth/ChangePasswordRequest.php
+++ app/Http/Requests/Auth/ChangePasswordRequest.php
@@ -22,7 +22,6 @@ class ChangePasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'identifier' => '',
             'password'   => 'required|string',
         ];
     }
